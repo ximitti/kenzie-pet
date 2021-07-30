55769b9521e4e217e435aaae349a739eba6ed12d
Alex Schmitt <alex.schmitt@outlook.com>
2021-07-29 21:19:01 -0300

    feature(wip): created all routes and verified request and parameter
    Need to implement process

--- animal/views.py
+++ animal/views.py
@@ -9,17 +9,23 @@ from .serializers import AnimalSerializer
 
 class AnimalView(APIView):
     def post(self, request):
+        # TODO: receber a request, separar por sessão(Group, Animal e Characteristic)
+        # TODO: para Group e Characteristic, verificar se existe, caso não criar
+        # TODO: retornar o animal criado, 201
         print(request.data)
         return Response({"message": "Hello, World!"})
 
     def get(self, request, animal_id=""):
+        # TODO: verificar se id não é vazio, se não for buscar pelo id
+        # TODO: senão listar toda a base
+        # TODO: verificar as formatações de retorno, 200
         animals = Animal.objects.all()
-        print(animals)
 
         serializer = AnimalSerializer(animals, many=True)
 
         return Response(serializer.data)
 
-    def delete(self, request):
+    def delete(self, request, animal_id=""):
+        # TODO: verificar se id existe, caso não retornar erro, senão deletar e retornar 204
         print(request.data)
         return Response({"message": "Hello, World!"})
