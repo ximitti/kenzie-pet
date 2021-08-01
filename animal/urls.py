aa9f11d3055df48166c39237c6de0c64ba07c558
Alex Schmitt <alex.schmitt@outlook.com>
2021-07-31 21:51:10 -0300

    feature: implemented routes logics and url routes

--- animal/urls.py
+++ animal/urls.py
@@ -6,5 +6,5 @@ from .views import AnimalView
 
 urlpatterns = [
     path("animals/", AnimalView.as_view()),
-    path("animals/<int:animal_id>", AnimalView.as_view()),
+    path("animals/<int:animal_id>/", AnimalView.as_view()),
 ]
--- animal/views.py
+++ animal/views.py
@@ -1,31 +1,77 @@
+from django.db.models import ObjectDoesNotExist
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Animal
+from group.models import Group
+from characteristic.models import Characteristic
+
 from .serializers import AnimalSerializer
 
 # ---------------------------------------
 
 
 class AnimalView(APIView):
-    def post(self, request):
-        # TODO: receber a request, separar por sessão(Group, Animal e Characteristic)
-        # TODO: para Group e Characteristic, verificar se existe, caso não criar
-        # TODO: retornar o animal criado, 201
-        print(request.data)
-        return Response({"message": "Hello, World!"})
+    def post(self, request) -> Response:
+        serialized = AnimalSerializer(data=request.data)
 
-    def get(self, request, animal_id=""):
-        # TODO: verificar se id não é vazio, se não for buscar pelo id
-        # TODO: senão listar toda a base
-        # TODO: verificar as formatações de retorno, 200
-        animals = Animal.objects.all()
+        if not serialized.is_valid():
+            return Response(serialized.error, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer = AnimalSerializer(animals, many=True)
+        animal_data = serialized.validated_data
+        group_data = animal_data.pop("group")
+        characteristics_data = animal_data.pop("characteristics")
 
-        return Response(serializer.data)
+        group: Group = Group.objects.get_or_create(**group_data)[0]
 
-    def delete(self, request, animal_id=""):
-        # TODO: verificar se id existe, caso não retornar erro, senão deletar e retornar 204
-        print(request.data)
-        return Response({"message": "Hello, World!"})
+        animal: Animal = Animal.objects.create(**animal_data, group=group)
+
+        characteristics: list[Characteristic] = [
+            Characteristic.objects.get_or_create(**characteristic)[0]
+            for characteristic in characteristics_data
+        ]
+
+        animal.characteristics.set(characteristics)
+
+        serialized = AnimalSerializer(animal)
+
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def get(self, request, animal_id="") -> Response:
+        if animal_id:
+            try:
+                animal: Animal = Animal.objects.get(id=animal_id)
+
+                serialized: AnimalSerializer = AnimalSerializer(animal)
+
+                return Response(serialized.data, status=status.HTTP_200_OK)
+
+            except ObjectDoesNotExist as _:
+                return Response(
+                    {"detail": "Animal not found"}, status=status.HTTP_404_NOT_FOUND
+                )
+
+        animals: list[Animal] = Animal.objects.all()
+
+        serialized: list[AnimalSerializer] = AnimalSerializer(animals, many=True)
+
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, animal_id="") -> Response:
+        try:
+            animal: Animal = Animal.objects.get(id=animal_id)
+
+            animal.delete()
+
+            return Response("", status=status.HTTP_204_NO_CONTENT)
+
+        except ObjectDoesNotExist as _:
+            return Response(
+                {"detail": "Animal not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        except ValueError as _:
+            return Response(
+                {"detail": "Missing parameter id"}, status=status.HTTP_400_BAD_REQUEST
+            )
